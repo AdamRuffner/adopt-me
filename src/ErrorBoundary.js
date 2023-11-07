f890e996d013e6a5b333c4d83b6553356e3bfdae
Adam <ruffneradam@aol.com>
2023-11-07 15:20:16 -0700

    updated parcel for build to run

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,35 +1,35 @@
 // mostly code from react.js.org/docs/error-boundaries.html
 
-// import React from "react";
-// import { Link, Redirect } from "@reach/router";
+import React from "react";
+import { Link, Redirect } from "@reach/router";
 
-// class ErrorBoundary extends Component {
-//   state = { hasError: false };
-//   static getDerivedStateFromError() {
-//     return { hasError: true, redirect: false };
-//   }
-//   componentDidCatch(error, info) {
-//     console.error("Error boundary caught an error", error, info);
-//   }
-//   componentDidUpdate() {
-//     if (this.state.hasError) {
-//       setTimeout(() => this.setState({ redirect: true }), 5000);
-//     }
-//   }
-//   render() {
-//     if (this.state.redirect) {
-//       return <Redirect to="/" />;
-//     }
-//     if (this.state.hasError) {
-//       return (
-//         <h1>
-//           There was an error with this listing. <Link to="/">Click here</Link>{" "}
-//           to go back to hom epage or wait 5 seconds.
-//         </h1>
-//       );
-//     }
-//     return this.props.children;
-//   }
-// }
+class ErrorBoundary extends Component {
+  state = { hasError: false };
+  static getDerivedStateFromError() {
+    return { hasError: true, redirect: false };
+  }
+  componentDidCatch(error, info) {
+    console.error("Error boundary caught an error", error, info);
+  }
+  componentDidUpdate() {
+    if (this.state.hasError) {
+      setTimeout(() => this.setState({ redirect: true }), 5000);
+    }
+  }
+  render() {
+    if (this.state.redirect) {
+      return <Redirect to="/" />;
+    }
+    if (this.state.hasError) {
+      return (
+        <h1>
+          There was an error with this listing. <Link to="/">Click here</Link>{" "}
+          to go back to hom epage or wait 5 seconds.
+        </h1>
+      );
+    }
+    return this.props.children;
+  }
+}
 
-// export default ErrorBoundary;
+export default ErrorBoundary;
